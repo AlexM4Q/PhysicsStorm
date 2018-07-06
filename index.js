7fbc2b29b481b6f8b7115c70fb92003f2edb459a
Alex Zhelonkin <zhelonkin.ru@gmail.com>
2018-07-06 12:14:51 +0400

    Внедрение Express

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-import {start} from "./src/logic.js";
+// import {start} from "./src/logic.js";
+//
+// start();
 
-start();
+const path = require('path');
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', hbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'src/views/layouts')
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'src/views'));
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'John'
+    })
+});
+
+app.listen(3001);
--- src/public/objects/entities/player.js
+++ src/public/objects/entities/player.js
@@ -1,4 +1,4 @@
-import Vector from "../../data/vector";
+import Vector from "../../../data/vector";
 
 export default class Player {
 
